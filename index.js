4c24d20cf3fe01fa5dedd3eb31115f0ee0eb0c36
Samu Hujanen <samu.hujanen@tuni.fi>
2021-12-03 14:14:21 +0200

    test

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ app.use(cors());
 
 const db = [{ name: "tiina" }, { name: "jack" }];
 
+let config = {
+  host: "mydb.tamk.fi",
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  connectionLimit: 10,
+};
+
+var pool = mysql.createPool(config);
+app.get("/", (req, res) => {
+  pool.query("SELECT * from location", (error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(results);
+    }
+  });
+});
+
 app.get("/names", (req, res) => {
   res.send(db);
 });
